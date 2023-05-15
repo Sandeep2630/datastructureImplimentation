771f077423248c559b6c186b76ef45c5e75d9ba8
sandeep2630 <katwal.sandeep28@gmail.com>
2023-05-15 18:05:13 +0530

    final commit of the day not much changed

--- binaryTree.js
+++ binaryTree.js
@@ -1,3 +1,4 @@
+console.log("i am running");
 class Node {
   constructor(val) {
     this.val = val;
@@ -19,18 +20,96 @@ b.left = d;
 b.right = e;
 c.right = f;
 
-const treeVal = (root) => {
-  if (root === null) return [null];
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+// const e = new Node("e");
+// const f = new Node("f");
+// const g = new Node("g");
+// const h = new Node("h");
+// const i = new Node("i");
+// const j = new Node("j");
+// const l = new Node("l");
+// const m = new Node("m");
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// e.right = h;
+// f.left = i;
+// f.right = j;
+// j.right = l;
+// l.right = m;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//    / \   / \
+//   g   h i   j
+//              \
+//               l
+//                \
+//                 m
+
+// const treeValBreadth = (root) => {
+//   if (root === null) return [null];
+//   const val = [];
+
+//   const stack = [root];
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+//     val.push(current.val);
+//     if (current.right) stack.push(current.right);
+//     if (current.left) stack.push(current.left);
+//   }
+//   return val;
+// };
+
+// console.log(treeVal(a));
+
+// const treevalbreadth = (root) => {
+//   if (root === null) return [];
+//   const val = [];
+//   const stack = [root];
+
+//   while (stack.length > 0) {
+//     let current = stack.pop();
+//     val.push(current.value);
+
+//     if (current.right) stack.push(current.right);
+//     if (current.left) stack.push(current.left);
+//   }
+//   return val;
+// };
+
+// console.log(treevalbreadth(a));
+
+const breadthVal = (root) => {
   const val = [];
 
-  const stack = [root];
-  while (stack.length > 0) {
-    const current = stack.pop();
+  let queue = [root];
+  while (queue.length > 0) {
+    const current = queue.shift();
     val.push(current.val);
-    if (current.right) stack.push(current.right);
-    if (current.left) stack.push(current.left);
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
   }
+
   return val;
 };
 
-console.log(treeVal(a));
+console.log(breadthVal(a));
