4811f8552f7f10398eab179c5efcb0ef89279f04
sandeep2630 <katwal.sandeep28@gmail.com>
2023-05-17 13:59:54 +0530

    did roman to number some test case failing

--- binarySearch.js
+++ binarySearch.js
@@ -1,21 +1,76 @@
-const arr = [1, 3, 5, 6, 7, 9, 10, 15];
+// const arr = [1, 3, 5, 6, 7, 9, 10, 15];
 
-function binarySearch(arr, val) {
-  let lower = 0;
-  let upper = arr.length - 1;
-  while (lower <= upper) {
-    let middle = Math.floor((lower + upper) / 2);
-    if (val === arr[middle]) return middle;
+// function binarySearch(arr, val) {
+//   let lower = 0;
+//   let upper = arr.length - 1;
+//   while (lower <= upper) {
+//     let middle = Math.floor((lower + upper) / 2);
+//     if (val === arr[middle]) return middle;
 
-    if (val > arr[middle]) {
-      lower = middle + 1;
-    }
+//     if (val > arr[middle]) {
+//       lower = middle + 1;
+//     }
 
-    if (val < arr[middle]) {
-      upper = middle - 1;
+//     if (val < arr[middle]) {
+//       upper = middle - 1;
+//     }
+//   }
+
+//   return -1;
+// }
+// console.log(binarySearch(arr, 7));
+
+// Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+
+// Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+//  Example 1:
+
+// Input: nums1 = [1,2,3], nums2 = [2,4]
+// Output: 2
+// Explanation: The smallest element common to both arrays is 2, so we return 2.
+
+// const arr = [1, 2, 3];
+// const arr2 = [2, 4];
+// function minCommon(arr, arr2) {
+//   let i = 0;
+//   let j = 0;
+
+//   while (i < arr.length && j < arr2.length) {
+//     if (arr[i] === arr[j]) return arr[i];
+//   }
+// }
+
+// console.log(minCommon());
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+const str = "VII";
+// console.log(str[0]);
+function toRoman(str) {
+  let val = 0;
+  const map = new Map([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
+
+  for (let i = 0; i < str.length; i++) {
+    if (map.has(str[i])) {
+      val += map.get(str[i]);
     }
   }
 
-  return -1;
+  return val;
 }
-console.log(binarySearch(arr, 7));
+
+console.log(toRoman(str));
--- binaryTree.js
+++ binaryTree.js
@@ -1,25 +1,33 @@
-console.log("i am running");
 class Node {
-  constructor(val) {
-    this.val = val;
+  constructor(value) {
+    this.value = value;
     this.left = null;
     this.right = null;
   }
 }
 
-let a = new Node("a");
-let b = new Node("b");
-let c = new Node("c");
-let d = new Node("d");
-let e = new Node("e");
-let f = new Node("f");
+// Create nodes
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+const g = new Node("g");
+const h = new Node("h");
+const i = new Node("i");
+const j = new Node("j");
 
+// Connect nodes to form the binary tree
 a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
-c.right = f;
-
+c.left = f;
+c.right = g;
+d.left = h;
+d.right = i;
+e.left = j;
 // class Node {
 //   constructor(value) {
 //     this.value = value;
@@ -98,18 +106,67 @@ c.right = f;
 
 // console.log(treevalbreadth(a));
 
-const breadthVal = (root) => {
-  const val = [];
+// const breadthVal = (root) => {
+//   const val = [];
 
-  let queue = [root];
+//   let queue = [root];
+//   while (queue.length > 0) {
+//     const current = queue.shift();
+//     val.push(current.val);
+//     if (current.left) queue.push(current.left);
+//     if (current.right) queue.push(current.right);
+//   }
+
+//   return val;
+// };
+
+// console.log(breadthVal(a));
+// df search
+// ['a', 'b', 'd', 'h', 'i', 'e', 'j', 'c', 'f', 'g']
+//         a
+//       /   \
+//      b     c
+//    /  \   /  \
+//   d    e  f   g
+//  / \
+// h   i
+//      \
+//       j
+// bf search
+// ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+// function dfs(root) {
+//   const arr = [];
+//   if (root === null) return arr;
+//   const stack = [root];
+
+//   while (stack.length > 0) {
+//     let current = stack.pop();
+//     arr.push(current.value);
+//     if (current.right) stack.push(current.right);
+//     if (current.left) stack.push(current.left);
+//   }
+
+//   return arr;
+// }
+
+// console.log(dfs(a));
+
+function bfs(root) {
+  const arr = [];
+  if (root === null) return arr;
+
+  const queue = [root];
+  console.log("okay");
   while (queue.length > 0) {
     const current = queue.shift();
-    val.push(current.val);
-    if (current.left) queue.push(current.left);
+
+    arr.push(current.value);
     if (current.right) queue.push(current.right);
+    if (current.left) queue.push(current.left);
   }
 
-  return val;
-};
+  return arr;
+}
 
-console.log(breadthVal(a));
+console.log(bfs(a));
